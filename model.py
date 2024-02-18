2689487e76471fa1255468971b4ac10f17841be4
dinhtracducanh <dinhtracducanh04092005@gmail.com>
2024-02-18 22:28:48 +0700

    train and save CNN model

---
+++ model.py
@@ -0,0 +1,109 @@
+import numpy as np
+import cv2
+import os
+import sys
+import tensorflow as tf 
+from sklearn.model_selection import train_test_split
+
+IMG_HEIGHT = 100
+IMG_WIDTH = 100
+EPOCHS = 10
+TEST_SIZE = 0.3
+RANDOM_STATE = 64
+
+def main():
+    if len(sys.argv) not in [2, 3]:
+        sys.exit("Usage: python model.py train_test_data [model.h5]")
+
+    img, label = load_data(sys.argv[1])
+
+    img = np.array(img)
+    label = np.array(label)
+
+    print("Img shape: ", img.shape)
+    print("Lable shape: ", label.shape)
+
+    x_train, x_test, y_train, y_test = train_test_split(img, label, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+    x_train = x_train/255.0
+    x_test = x_test/255.0
+    y_train = tf.keras.utils.to_categorical(y_train)
+    y_test =  tf.keras.utils.to_categorical(y_test)
+
+    print("x_train shape: ", x_train.shape)
+    print("y_train shape: ", y_train.shape)
+    print("x_test shape: ", x_test.shape)
+    print("y_test shape: ", y_test.shape)
+
+    model = get_model()
+
+    model.fit(x_train, y_train, epochs = EPOCHS)
+
+    model.evaluate(x_test, y_test, verbose = 2)
+
+    if len(sys.argv) == 3:
+        filename = sys.argv[2]
+        model.save(filename)
+        print(f"Model saved to {filename}")
+
+def load_data(datadir):
+    current = os.getcwd()
+    data_folder = os.path.join(current, datadir)
+    try:
+        os.listdir(data_folder)
+    except:
+        sys.exit("The data folder for trainning does not exist")
+    if datadir != "train_test_data":
+        sys.exit("Usage: python model.py train_test_data [model.h5]")
+    img = []
+    label = []
+
+    for image in os.listdir(data_folder):
+        path = os.path.join(data_folder, image)
+        pic = cv2.imread(path,cv2.IMREAD_COLOR)
+        img.append(pic)
+        if image[:4] == "None":
+            label.append(0)
+        elif image[:4] == "Tube":
+            label.append(1)
+    print("successfully loaded images")
+    return img, label
+
+def get_model():
+    model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(
+        30, (3, 3), activation="relu", input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)
+    ),
+
+    tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
+
+    tf.keras.layers.Conv2D(
+        30, (3, 3), activation="relu", input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)
+    ),
+
+    tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
+
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(130, activation="relu"),
+
+    # tf.keras.layers.Dropout(0.2),
+
+    tf.keras.layers.Dense(100, activation="relu"),
+
+    tf.keras.layers.Dropout(0.2),
+
+    tf.keras.layers.Dense(70, activation = "relu"),
+
+    tf.keras.layers.Dropout(0.2),
+
+    tf.keras.layers.Dense(2, activation="softmax")
+    ])
+
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    
+    model.summary()
+
+    return model
+
+if __name__ == "__main__":
+    main()
